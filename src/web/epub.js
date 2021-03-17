706b14af1394afdb89edcc520c3adf1cb498020f
lkcv <48024577+lkcv@users.noreply.github.com>
2021-03-17 19:07:17 -0400

    Fix RTL navigation for pre-paginated books

--- src/web/epub.js
+++ src/web/epub.js
@@ -5764,7 +5764,7 @@ class default_DefaultViewManager {
           next = this.views.last().section.next();
         }
       } else {
-        left = this.container.scrollLeft + this.layout.delta * -1;
+        left = this.container.scrollLeft - this.container.offsetWidth;
 
         if (left > this.container.scrollWidth * -1) {
           this.scrollBy(this.layout.delta, 0, true);
